861cacc21cbed002f0846ff7cb1f211b09d71acc
Harshita <guptaharshita99@gmail.com>
2020-09-20 10:35:57 +0000

    Add some styling

--- src/recipe.js
+++ src/recipe.js
@@ -1,13 +1,39 @@
 import React,{useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const Recipe=(props)=>{
-  return(
-<div>
-<h1>{props.title}</h1>
-<p><strong> Calories contain: </strong>{props.calories}</p>
-<img src={props.image} alt="recipe image"/>
-</div>
+  const [ingredients,setIngredients]=useState([])
+//console.log(props)
+const viewIngredients=()=>{
 
-  )
+  setIngredients(props.ingredients)
+}
+const ing = props.ingredients
+
+  return(
+<div className='list'>
+
+<div className="card card-style">
+    <div className="card-image waves-effect waves-block waves-light">
+      <img  src={props.image} alt="recipe"/>
+    </div>
+    <div className="card-content">
+      <span className="card-title activator grey-text text-darken-4">{props.title}<i className="material-icons right activator">more_vert</i></span>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">{props.title}<i class="material-icons right">close</i></span>
+      <p> 
+          <ul>
+          {ing.map((ing)=><li>{ing}</li>)}
+          </ul>
+      
+      </p>
+    </div>
+
+    
+  </div>
+
+</div>
+  );
 }
 export default Recipe;
